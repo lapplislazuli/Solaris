f639f91da02344c441ca77a6f82a3b785a3bb822
Twonki <l.gutschon@web.de>
2018-09-23 18:05:21 +0200

    DistanceTest passes

--- test/core/DistanceTest.java
+++ test/core/DistanceTest.java
@@ -1,20 +1,20 @@
-/**
- * @Author Leonhard Applis
- * @Created 03.09.2018
- * @Package space.core
- */
-package space.core;
+package core;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import geom.Circle;
+import geom.Point;
+import space.core.Planet;
+import space.core.Star;
+
 class DistanceTest {
 	
 	@Test
 	void positiveDistance() {
-		Star anker= new Star("anker", null, 0, 0, 0);
+		Star anker= new Star("anker", null, new Point( 0, 0), new Circle(new Point(0,0), 0));
 		Planet a = new Planet("a", anker, null, 0, 0, 0);
 		Planet b = new Planet("b", anker, null, 0, 50, 0);
 		Planet c = new Planet("c", anker, null, 0, 100, 0);
@@ -27,7 +27,7 @@ class DistanceTest {
 	
 	@Test
 	void negativeDistance() {
-		Star anker= new Star("anker", null, 150, 0, 0);
+		Star anker= new Star("anker", null, new Point(150, 0), new Circle(new Point(150,0), 0));
 		Planet a = new Planet("a", anker, null, 0, 0, 0);
 		Planet b = new Planet("b", anker, null, 0, -50, 0);
 		Planet c = new Planet("c", anker, null, 0, -100, 0);
@@ -40,9 +40,8 @@ class DistanceTest {
 	
 	@Test
 	void nullDistance() {
-		Star anker1= new Star("anker1", null, 0, 0, 0);
-		Star anker2= new Star("anker2", null, 0, 0, 0);
-		
+		Star anker1= new Star("anker", null, new Point( 0, 0), new Circle(new Point(0,0), 0));
+		Star anker2= new Star("anker", null, new Point( 0, 0), new Circle(new Point(0,0), 0));
 		assertEquals(0,anker1.distanceTo(anker2));
 		assertEquals(true, anker1.distanceTo(anker2)==anker2.distanceTo(anker1));
 	}
