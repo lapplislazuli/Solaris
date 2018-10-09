74f7362bddf61b1b6ffc9e8498a4282ee79cc669
Twonki <l.gutschon@web.de>
2018-10-09 16:26:21 +0200

    improved Outline Tests closes #53

--- test/geom/RectangleTest.java
+++ test/geom/RectangleTest.java
@@ -5,39 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import interfaces.geom.Point;
 
 class RectangleTest {
 	
-	private static Rectangle testObject;
-	private static AbsolutePoint centerTestObject;
+	private Rectangle testObject;
+	private AbsolutePoint centerTestObject;
 	
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	@BeforeEach
+	void setUpBeforeClass() throws Exception {
 		centerTestObject= new AbsolutePoint(100,100);
 		testObject = new Rectangle(centerTestObject,200,200);
 	}
 	
-	//TODO: Write Better Test!
-	@Test
-	void testInitOutline() {
-		testObject.levelOfDetail=2; //4 OutlinePoints!
-		testObject.updateOrInitOutline(); 
-		//System.out.println(testObject.outLine.toString());
-		/*for (for Point ) {
-			Point testOutlinePoint=testObject.outLine.get(i);
-			assertTrue(
-				testOutlinePoint.distanceTo(new AbsolutePoint(0,200))==0
-				|| testOutlinePoint.distanceTo(new AbsolutePoint(200,200))==0
-				|| testOutlinePoint.distanceTo(new AbsolutePoint(200,0))==0
-				|| testOutlinePoint.distanceTo(new AbsolutePoint(0,0))==0
-			);
-		}*/
-	}
-
 	@Test
 	void testPositiveContains() {
 		List<AbsolutePoint> inPoints = new LinkedList<AbsolutePoint>();
@@ -51,8 +34,8 @@ class RectangleTest {
 	void testNegativeContains() {
 		List<AbsolutePoint> outPoints = new LinkedList<AbsolutePoint>();
 		outPoints.add(new AbsolutePoint(0,300));  
-		outPoints.add(new AbsolutePoint(0,-10)); 
-		//outPoints.add(new Point(0,0,400)); 
+		outPoints.add(new AbsolutePoint(0,-10));
+		
 		for(AbsolutePoint outPoint : outPoints)
 			assertFalse(testObject.contains(outPoint));
 	}
@@ -61,5 +44,34 @@ class RectangleTest {
 	void testTangentialContains() {
 		Point edgePoint=new AbsolutePoint(200,200); 
 		assertTrue(testObject.contains(edgePoint));
+	}
+
+	@Test
+	void testInitOutlineWithRelativeRectangles() {
+		Rectangle biggerRectangle = new Rectangle(centerTestObject, 202,202);
+		Rectangle smallerRectangle = new Rectangle(centerTestObject, 190, 190);
+		
+		testObject.setLevelOfDetail(100);
+		testObject.initOutline();
+		
+		for(Point p : testObject.outLine) {
+			assertTrue(biggerRectangle.contains(p));
+			assertFalse(smallerRectangle.contains(p));
+		}
+	}
+	
+	@Test
+	void testOutlineIsTangenting() {
+		testObject.setLevelOfDetail(100);
+		testObject.initOutline();
+		
+		for(Point p : testObject.outLine)
+			testObject.contains(p);
+	}
+	@Test
+	void testOutlineCount() {
+		testObject.levelOfDetail=25;
+		testObject.updateOrInitOutline();
+		assertEquals(100,testObject.outLine.size());
 	}
 }
