efb3a88c9f8280567ab5f650b8bdcce46275ba49
Twonki <l.gutschon@web.de>
2018-10-11 20:29:45 +0200

    Added Basics for JavaFXDrawingInfo

--- src/space/effect/Effect.java
+++ src/space/effect/Effect.java
@@ -1,19 +1,21 @@
 package space.effect;
 
 import geom.AbsolutePoint;
+import interfaces.drawing.ComplexDrawingObject;
 import interfaces.drawing.DrawingContext;
-import interfaces.drawing.DrawingObject;
+import interfaces.drawing.DrawingInformation;
 import interfaces.geom.Point;
 import interfaces.geom.Shape;
 import interfaces.logical.RemovableObject;
 import interfaces.logical.UpdatingObject;
 import logic.EffectManager;
 
-public abstract class Effect implements UpdatingObject, DrawingObject, RemovableObject{
+public abstract class Effect implements UpdatingObject, ComplexDrawingObject, RemovableObject{
 	
 	String name;
 	Point center;
 	Shape shape;
+	DrawingInformation dInfo;
 	
 	public Effect(String name,Point p, Shape s){
 		this.name = name;
@@ -29,12 +31,21 @@ public abstract class Effect implements UpdatingObject, DrawingObject, Removable
 	public void update() {}
 	
 	public void draw(DrawingContext dc) {
+		
+		drawShape(dc);
+		dc.resetContext();
+	}
+	
+	public void drawShape(DrawingContext dc) {
 		shape.draw(dc);
 	}
 	
-	
 	public boolean isCovered(int x, int y) {
 		return shape.contains(new AbsolutePoint(x,y));
+	}
+	
+	public DrawingInformation getDrawingInformation() {
+		return dInfo;
 	}
 	
 	@Override
