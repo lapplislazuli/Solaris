3e443b9f3f36e34b1d31dd6fd369f255e1937fce
Twonki <l.gutschon@web.de>
2018-10-14 14:13:18 +0200

    Added JsonJar to new Folder Lib

---
+++ src/config/KeyManagerConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.*;
+
+public class KeyManagerConfig {
+	
+	private Map<String,String> keyBindings = new HashMap<String,String>();
+	
+}
